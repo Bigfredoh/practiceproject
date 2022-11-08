cbda73fa2c7fcb10e627b15f734717c635a4c6e0
Friday Ekpe <fridayekpe498@gmail.com>
2022-11-08 09:27:28 +0100

    created epl crawler

--- nl/nl/spiders/nl_spider.py
+++ nl/nl/spiders/nl_spider.py
@@ -1,26 +1,21 @@
 # Importing of the necessary libraries
 import scrapy
 from scrapy import Request
-
 # Building of scrapy framework for parsing the website
 class TotalspiderSpider(scrapy.Spider):
     name = 'nairaland'
     current_page = 1
     start_urls = ['https://www.nairaland.com/news/']
-
 # Extracting links from all the pages
     def parse(self, response):
         links =  response.css('[summary="links"] a::attr(href)').getall()
         for link in links:
             yield Request(url=link, callback=self.parse_categories)
-
-
         total_pages = int(response.css('div.body p:nth-child(6) b:last-child::text').get())
         url = 'https://www.nairaland.com/news/' + str(TotalspiderSpider.current_page)
         if TotalspiderSpider.current_page <= total_pages:
             TotalspiderSpider.current_page += 1
             yield Request (url=url, callback=self.parse)
-
 # Parsing of the website
     def parse_categories(self, response):
         news_container = response.css('div.body')
